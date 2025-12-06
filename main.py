b2e8c533617d4b52bd65877280ba8341a83158ed
pflanagan <pflanagan@grubhub.com>
2025-12-06 13:44:05 -0500

    file name in params

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from src.generate import generate
 
 if __name__ == "__main__":
   if len(sys.argv) < 3:
-    print("Usage: python3 ./src/generate.py <background|radial_background|image> <params>")
+    print("Usage: python3 ./src/generate.py <background|radial_background|image> fileName <params>")
     sys.exit(1)
   
   generateType = sys.argv[1]
-  params = sys.argv[2:]
-  generate(generateType, 'out', generateType, *params)
+  outputName = sys.argv[2]
+  params = sys.argv[3:]
+  generate(generateType, 'out', outputName, *params)
